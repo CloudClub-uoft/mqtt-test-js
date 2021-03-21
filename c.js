af874290e3b8a356487b0a794e42693b9bd677f7
Harsimrat <singh21.h@gmail.com>
2021-03-20 20:22:00 -0400

    initial

---
+++ c.js
@@ -0,0 +1,26 @@
+const mqtt = require('mqtt');
+const mqttClient = mqtt.connect('mqtt://localhost');
+
+const queryTopic = 'resolveMyQuery';
+const responseTopic = 'responseFromServer';
+
+mqttClient.on('message', function(topic, message) {
+    // message is Buffer
+    console.log('Received response from server:-', message.toString())
+    mqttClient.end();
+});
+
+mqttClient.on('connect', function() {
+    console.log('Client connected to Mqtt broker');
+    // Subscribe to the response topic 
+    mqttClient.subscribe(responseTopic);
+    // Publish message
+    mqttClient.publish(queryTopic, 'Hello server, can you hear me?');
+    console.log('Published to server...');
+
+});
+
+
+// printNumbers(5, 10);
+// mqtt test command. Connects to the mosquitto MQTT test server.
+// mosquitto_sub -h test.mosquitto.org -t "#" -v
---
+++ index.js
@@ -0,0 +1,20 @@
+const mqtt = require('mqtt');
+//mqtt://broker.hivemq.com
+const mqttClient = mqtt.connect('mqtt://localhost');
+
+const queryTopic = 'resolveMyQuery';
+const responseTopic = 'responseFromServer';
+
+mqttClient.on('connect', function() {
+    console.log('Server connected to Mqtt broker');
+    mqttClient.subscribe(queryTopic);
+});
+
+// On receiving message from any client
+mqttClient.on('message', function(topic, message) {
+    console.log('Received query from client: -', message.toString());
+    // Responding to client
+    mqttClient.publish(responseTopic, 'Hello client, yes I can hear you');
+    console.log('Responded to client');
+    mqttClient.end();
+});
